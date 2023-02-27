7a036dd005d4e8232ac173ba0c2858efa013b7a3
beam2546 <beam2546@users.noreply.github.com>
2023-02-27 14:20:57 +0700

    Finished Valid Palindrome Problem

---
+++ valid-palindrome.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"regexp"
+	"strings"
+)
+
+func clearStringAndSpace(str string) string {
+	reg, _ := regexp.Compile("[^a-zA-Z0-9]+")
+	return reg.ReplaceAllString(str, "")
+}
+
+func isPalindrome(s string) bool {
+	s = strings.ToLower(s)
+	s = clearStringAndSpace(s)
+	for i := 0; i < len(s)/2; i++ {
+		if s[i] != s[len(s)-1-i] {
+			return false
+		}
+	}
+
+	return true
+}
+
+func main() {
+	print(isPalindrome("A man, a plan, a canal: Panama"))
+}
